17c4389363de9c8ddb89332708359a5ffb1b0de2
YunSeok Rho <bamgil999@gmail.com>
2023-11-29 23:06:34 +0900

    25:40

--- app/src/main/java/com/example/test/Adapters/HourlyAdapters.java
+++ app/src/main/java/com/example/test/Adapters/HourlyAdapters.java
@@ -1,12 +1,17 @@
 package com.example.test.Adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.test.Domains.Hourly;
+import com.example.test.R;
 
 import java.util.ArrayList;
 
@@ -23,19 +28,31 @@ public class HourlyAdapters extends RecyclerView.Adapter<HourlyAdapters.viewhold
     @Override
     public HourlyAdapters.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         context=parent.getContext();
-        return null;
+        View inflater= LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_hourly,parent,false)
+        return new viewholder(inflate);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HourlyAdapters.viewholder holder, int position) {
-
+holder.hourTxt.setText(items.get(position).getHour());
+holder.tempTxt.setText(items.get(position).getTemp()+""); //2023.11.29 23:05
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return items.size();
     }
 
-    public class viewholder {
+    public class viewholder extends RecyclerView.ViewHolder {
+        TextView hourTxt,tempTxt;
+        ImageView pic;
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+            hourTxt=itemView.findViewById(R.id.hourTxt);
+            tempTxt=itemView.findViewById(R.id.tempTxt);
+            pic=itemView.findViewById(R.id.pic);
+
+        }
     }
 }
