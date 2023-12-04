2bff7269e349d0588e97b51d654ec1869e58c0c8
YunSeok Rho <bamgil999@gmail.com>
2023-12-04 10:05:28 +0900

    초기페이지

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,12 +30,18 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    //1.6.1
+    implementation("com.google.android.material:material:1.8.0")
+    //1.10.1
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    //2.1.4
     testImplementation("junit:junit:4.13.2")
+    //4.13.2
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    //1.1.5
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //3.5.1
 
     //??
     implementation("com.github.bumptech.glide:glide:4.12.0")
