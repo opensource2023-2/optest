62c9c1fc0e7c910a482a80b7cbaece97d5908df0
YunSeok Rho <bamgil999@gmail.com>
2023-12-05 20:03:56 +0900

    complete first page touch button

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btn_move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_hello_page);
+        setContentView(R.layout.activity_main);
+        btn_move=findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent= new Intent(getApplicationContext(), Login_page.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
