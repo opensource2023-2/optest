a849ec9ed0cb1fd12ec18fd73ca3c256f2903c85
YunSeok Rho <bamgil999@gmail.com>
2023-12-06 03:08:29 +0900

    add png file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,20 +32,12 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.5.1")
-    //1.6.1
     implementation("com.google.android.material:material:1.8.0")
-    //1.10.1
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
-    //2.1.4
     testImplementation("junit:junit:4.13.2")
-    //4.13.2
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    //1.1.5
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    //3.5.1
-
-    //??
     implementation("com.github.bumptech.glide:glide:4.12.0")
 }
