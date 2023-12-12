223b7fd4deb72d93e10f2eae278441bd0a09ae09
hyejun <127915444+nojungsin@users.noreply.github.com>
2023-12-12 15:06:18 +0900

    alllayoutbasic

---
+++ app/src/main/java/com/example/test/clothes_mat_ex.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class clothes_mat_ex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothes_mat_ex);
+    }
+}
