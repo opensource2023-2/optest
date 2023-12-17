5f1c1093c5b536505752ac7770377495de3be20a
anchihyeon2 <alwaysis0613@gmailcom>
2023-12-17 15:44:28 +0900

    hello page 디자인 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation 'com.google.android.material:material:1.4.0'
+
 }
