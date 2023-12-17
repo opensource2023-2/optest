2921c4872414b18fe2a6f5e65882490d4f5bc987
anchihyeon2 <alwaysis0613@gmailcom>
2023-12-17 16:11:58 +0900

    페이지 기본 툴만 완성

--- app/src/main/java/com/example/test/Hello_page.java
+++ app/src/main/java/com/example/test/Hello_page.java
@@ -9,6 +9,6 @@ public class Hello_page extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello_page);
+        setContentView(R.layout.activity_whether_rec_week_page);
     }
 }
--- app/src/main/java/com/example/test/Intro_page.java
+++ app/src/main/java/com/example/test/Intro_page.java
@@ -3,7 +3,6 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -14,7 +13,7 @@ public class Intro_page extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro_page);
+        setContentView(R.layout.activity_home_page);
 
         mat_btn=findViewById(R.id.mat_btn);
         mat_btn.setOnClickListener(new View.OnClickListener(){
